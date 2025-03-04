8eff24df576297ccafd435457a2c83e24d922222
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2025-03-04 09:54:58 +0100

    Dev : Flappy Paper
    - Not 100% functionnal

--- app/src/main/java/com/example/minipaper/MainActivity.kt
+++ app/src/main/java/com/example/minipaper/MainActivity.kt
@@ -23,7 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         val soloButton = findViewById<ImageView>(R.id.imageView9)
         soloButton.setOnClickListener {
-            val intent = Intent(this, CountdownActivity::class.java)
+            //val intent = Intent(this, CountdownActivity::class.java) temporarly disabled
+            val intent = Intent(this, FlappyPapierActivity::class.java)
             startActivity(intent)
         }
 
@@ -38,5 +39,6 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LeaderbordActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
